51a1306655d5342dd9929a31231285a0a1a297a6
Cristian Montes <cmontesmujica@gmail.com>
2021-08-11 16:24:12 -0700

    home,data and id endpoints working

---
+++ data/cows.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    id: 1,
+    sex: 'male',
+    number_horns: 11,
+    milk: false,
+    cow_breed: 'Sebu',
+  },
+  {
+    id: 2,
+    sex: 'female',
+    number_horns: 22,
+    milk: true,
+    cow_breed: 'charolais',
+  },
+  {
+    id: 3,
+    sex: 'male',
+    number_horns: 22,
+    milk: true,
+    cow_breed: 'Sebu',
+  },
+  {
+    id: 4,
+    sex: 'female',
+    number_horns: 22,
+    milk: false,
+    cow_breed: 'charolais',
+  }
+];
--- data/create-tables.js
+++ data/create-tables.js
@@ -17,10 +17,12 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE cows (
                     id SERIAL PRIMARY KEY NOT NULL,
-                    name VARCHAR(512) NOT NULL,
-                    cool_factor INTEGER NOT NULL,
+                    sex VARCHAR(512) NOT NULL,
+                    number_horns INTEGER NOT NULL,
+                    milk BOOLEAN NOT NULL,
+                    cow_breed VARCHAR(512) NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
